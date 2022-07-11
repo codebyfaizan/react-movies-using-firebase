cce0657c33e672e6fd4061c1964e7db8b9292af3
faizan-aka <faizan.lko888@gmail.com>
2022-07-11 21:30:22 +0530

    Updates

---
+++ precache-manifest.684c4e9516e3dff1f90a5760ace5d431.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be413247c1a0a4b936f4",
+    "url": "/react-movies-using-firebase/static/css/main.62b921c4.chunk.css"
+  },
+  {
+    "revision": "be413247c1a0a4b936f4",
+    "url": "/react-movies-using-firebase/static/js/main.be413247.chunk.js"
+  },
+  {
+    "revision": "42ee0468eda350be5eeb",
+    "url": "/react-movies-using-firebase/static/js/1.42ee0468.chunk.js"
+  },
+  {
+    "revision": "29b984619696943bfd3e",
+    "url": "/react-movies-using-firebase/static/js/runtime~main.29b98461.js"
+  },
+  {
+    "revision": "2a57c6e96e36ffde7027167317065108",
+    "url": "/react-movies-using-firebase/index.html"
+  }
+];
